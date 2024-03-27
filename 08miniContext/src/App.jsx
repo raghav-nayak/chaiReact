0a22fb76b5b1e4ea40ccdcb03dfab44c1dd1ee7e
Raghav Nayak <ragsnayak@gmail.com>
2024-03-28 00:02:32 +0530

    added user context for login and profile

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,13 +1,16 @@
-import { useState } from 'react'
-import './App.css'
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import UserContextProvider from "./context/UserContextProvider";
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1> React context demo project </h1>
-    </>
+    <UserContextProvider>
+        <h1> React context demo project </h1>
+            <Login />
+            <Profile />
+    </UserContextProvider>
   )
 }
 
